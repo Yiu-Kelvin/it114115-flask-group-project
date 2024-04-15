d4f6dc826d5472916b7e97dec7eace45784b18a9
nkw121 <230059448@stu.vtc.edu.hk>
2024-04-15 23:00:11 +0800

    added ignored_tag, followed_tag

--- app/config.py
+++ app/config.py
@@ -5,8 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+    if os.environ.get('testdb') == "True": 
+        SQLALCHEMY_DATABASE_URI = "sqlite://"
+    else:
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
@@ -14,4 +20,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['flask@pikaamail.com']
     POSTS_PER_PAGE = 3
+    TAGS_PER_PAGE = 36
     LANGUAGES = ['en', 'es', 'zh']
--- app/models.py
+++ app/models.py
@@ -18,7 +18,16 @@ post_tags = db.Table(
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
     db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
 )
-
+followed_tags = db.Table(
+    'followed_tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+ignored_tags = db.Table(
+    'ignored_tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -27,12 +36,19 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed_tags = db.relationship('Tag', secondary=followed_tags, lazy='dynamic', backref=db.backref('followed_by_user', lazy='dynamic'))
+    ignored_tags = db.relationship('Tag', secondary=ignored_tags, lazy='dynamic', backref=db.backref('ingored_by_user', lazy='dynamic'))
+
+    
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-
+    
+    def post_without_ignored(self):
+        return Post.query.filter(~Post.tags.any(Tag.id.in_([tag.id for tag in self.ignored_tags]))).order_by(Post.created_at.desc())
+    
     def __repr__(self) -> str:
         return f'<User {self.username}>'
 
@@ -46,7 +62,35 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+    
+    def is_following_tag(self, tag):
+        return self.followed_tags.filter(followed_tags.c.tag_id == tag.id).count() > 0
+    
+    def toggle_follow_tag(self, tag):
+        if not self.is_following_tag(tag):
+            self.stop_ignoring_tag(tag)
+            self.followed_tags.append(tag)
+        else:
+            self.followed_tags.remove(tag)
+            
+    def unfollow_tag(self, tag):
+        if self.is_following_tag(tag):
+            self.followed_tags.remove(tag)
 
+    def toggle_ignore_tag(self, tag):
+        if not self.is_ignoring_tag(tag):
+            self.unfollow_tag(tag)
+            self.ignored_tags.append(tag)
+        else:
+            self.ignored_tags.remove(tag)
+
+    def stop_ignoring_tag(self, tag):
+        if self.is_ignoring_tag(tag):
+            self.ignored_tags.remove(tag)
+
+    def is_ignoring_tag(self, tag):
+        return self.ignored_tags.filter(ignored_tags.c.tag_id == tag.id).count() > 0
+            
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -63,7 +107,7 @@ class User(UserMixin, db.Model):
             followers, followers.c.followed_id == Post.user_id
         ).filter(followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
-        return followed.union(own).order_by(Post.timestamp.desc())
+        return followed.union(own).order_by(Post.created_at.desc())
 
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode({"reset_password": self.id,
