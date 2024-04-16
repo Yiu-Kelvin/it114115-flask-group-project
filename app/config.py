e39b1ef78a80216a86e18775adb5c23c41ce0079
Peterop-Chan <230046521@stu.vtc.edu.hk>
2024-04-16 09:58:54 +0800

    added tests, post_vote, answer_vote , answer

--- app/config.py
+++ app/config.py
@@ -5,7 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+    if os.environ.get('testdb') == "True": 
+        SQLALCHEMY_DATABASE_URI = "sqlite://"
+    else:
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:1234@flask-db:3306/flaskdb'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
